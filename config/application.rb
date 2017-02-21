b757120f491820e1ae098d1bd76210ec11247706
rhythm191 <sac2ndg@gmail.com>
2017-02-21 20:26:01 +0900

    generatorの設定を外出しする

--- config/application.rb
+++ config/application.rb
@@ -29,10 +29,5 @@ module IsDayOff
 
     # active job
     config.active_job.queue_adapter = :sucker_punch
-
-    config.generators do |g|
-      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
-      g.fixture_replacement :factory_girl, dir: 'spec/support/factories'
-    end
   end
 end

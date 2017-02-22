d7ce2736a474f0f2ad71d83d7655b65340d1795b
rhythm191 <sac2ndg@gmail.com>
2017-02-22 20:37:44 +0900

    日付のrange判定を分離

--- app/controllers/day_off_controller.rb
+++ app/controllers/day_off_controller.rb
@@ -2,32 +2,32 @@ require 'date'
 
 class DayOffController < ApplicationController
 
+  before_action :bad_request_if_out_of_range, only: :is
+
   def today
     day_off = DayOff.new(Time.now, get_country, I18n.locale)
     render json: day_off
   end
 
   def is
-    date = Date.strptime(params[:day],'%Y-%m-%d')
-
-    # 現在から1年前後でなければOutOfRangeExceptionをだす
-    bad_request_if_out_of_range(date.to_time) and return
-
-    day_off = DayOff.new(date.to_time, get_country, I18n.locale)
+    day_off = DayOff.new(get_date.to_time, get_country, I18n.locale)
     render json: day_off
   end
 
 
   private
+  def get_date
+    Date.strptime(params[:day],'%Y-%m-%d')
+  end
+
   def get_country
     params['country'] || 'japanese'
   end
 
   # 現在から1年前後でなければOutOfRangeExceptionをだす
-  # @param [Time] time
-  def bad_request_if_out_of_range(time)
-    time_range = (Time.now - 1.years)..(Time.now + 1.years)
-    unless time_range.include? time
+  def bad_request_if_out_of_range
+    date_range = (Date.today - 1.years)..(Date.today + 1.years)
+    unless date_range.include? get_date
       render json: {exception: 'OutOfRangeException', description: I18n.t('error.outofrange.description') }, status: :bad_request
     end
   end

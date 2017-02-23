e147eb1111b003542395de4cc5a5822c943bf746
rhythm191 <sac2ndg@gmail.com>
2017-02-23 20:32:04 +0900

    インデックスの削除をした

--- db/migrate/20170220121442_add_locale_to_holiday.rb
+++ db/migrate/20170220121442_add_locale_to_holiday.rb
@@ -1,6 +1,7 @@
 class AddLocaleToHoliday < ActiveRecord::Migration[5.0]
   def change
     add_column :holidays, :locale, :string
+    remove_index :holidays, :date
     add_index :holidays, [:date, :country, :locale]
   end
 end
